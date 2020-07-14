4b31886696280af31e249f0ce83ca2091fa5b759
Ethan Yu <sohbmpethan@gmail.com>
2020-07-14 15:45:10 +0900

    01-first-program

---
+++ 01-first-program/first_program.m
@@ -0,0 +1,22 @@
+%% This section does some simple arithmetic
+
+% %% creates a cell block which could be run separately from the whole code
+
+% Single-line comment
+
+a = 4 + ... comment in the expression
+6
+
+disp(a)
+
+%{
+Multiline comments
+Multiline comments
+Multiline comments
+%}
+1+1
+
+% Comment: ?-/
+% Uncomment: ?-T
+% Run a piece of code??R
+% Run all code: ??R
