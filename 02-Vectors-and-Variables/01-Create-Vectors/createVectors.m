2956991d67dc7cc295ddf6bd65648193adede99d
Ethan Yu <sohbmpethan@gmail.com>
2020-08-26 17:03:18 +0900

    01-Create-Vectors

---
+++ 02-Vectors-and-Variables/01-Create-Vectors/createVectors.m
@@ -0,0 +1,30 @@
+%%
+
+% make a row vector using square brackets
+vectR = [ 1 2 3 4 2 3 4 5 6 -111];
+
+% make a column vector using semicolons
+vectC = [ 2; 4; 6; 8; 20; 30];
+
+% make a column vector by transposing a row vector
+vect = [ 1 2 3 4 5 6 7 7 8 9 0 10]';
+vect = transpose( [1 2 3 4 5 6]);
+
+% create a 2x3 matrix
+matr = [ 1 2 3; 4 5 6 ];
+
+% or transpose a 3x2 matrix!
+matr = [ 1 2; 4 5; 7 8 ];
+
+% make a row vector of ones
+onesvect = ones(5,1);
+
+% column vector of .25
+vect = ones(5,1) * 0.25;
+
+% matrix of random numbers
+matr = randn(4, 3);
+
+% transpose that matrix and store as a new matrix
+
+%%
