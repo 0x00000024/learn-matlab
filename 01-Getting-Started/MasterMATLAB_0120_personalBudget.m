ded817bc260132763147704079c0fd70691ce3ae
Ethan Yu <sohbmpethan@gmail.com>
2020-08-26 15:39:01 +0900

    rename

---
+++ 01-Getting-Started/MasterMATLAB_0120_personalBudget.m
@@ -0,0 +1,37 @@
+%% 
+%     COURSE: Master MATLAB through guided problem-solving
+%    SECTION: Getting started
+%      VIDEO: Using MATLAB for a personal budget
+% Instructor: mikexcohen.com
+%
+%%
+
+% monthly income (post-tax)
+income = 2500;
+
+
+% list monthly expenses
+rent  = 1200;
+utils =  300; % water, electricity, internet, etc.
+car   =  250; % gas and insurance
+food  =  300; % assuming 75/week
+phone =   50; % gotta have unlimited downloads!
+retirement = ; % you should be saving 10% of your income!
+
+
+% total monthly expenditures
+outflow = rent + utils + car + food + phone + retirement;
+
+% amount left over for nonessential expenses
+nonessentials = 
+
+
+% how much can you spend per day?
+perday = 
+disp([ 'I can spend '  ' extra each day.' ])
+
+
+% what if you spend twice as much during the weekend as during the week?
+
+
+%%
